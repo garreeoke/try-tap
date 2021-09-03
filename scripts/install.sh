72a3a80ab8a77df49608ee5a3d26f616d36955ad
Aaron Torgerson <taaron@taaron-a01.vmware.com>
2021-09-02 19:59:07 -0700

    debug

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,8 @@ KBLD_VERSION="v0.30.0"
 KUBECTL_VERSION=v1.22.0
 UAA_REFRESH_TOKEN="593092fe98754de894f37d4bea612bc4-r" # Make an option
 TANZU_CLI_VERSION="v1.4.0-rc.5" # Make an option
+TANZU_NET_USER="taaron@vmware.com"
+TANZU_NET_PASS="GwRnBa0#"
 
 ### Load Helper Functions
 . ./scripts/functions.sh
@@ -36,17 +38,21 @@ install_tanzu_cli "$UAA_REFRESH_TOKEN" "$TANZU_CLI_VERSION"
 
 ### Set up Kubernetes environment
 info "Done for now ..."
-exit 0
 install_k3s
+# Install kapp controller
+echo "Install kapp controller"
+kapp deploy -a kc -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml
+# Setup package repo
+kubectl create namespace tap-install
+kubectl create secret docker-registry tap-registry -n tap-install --docker-server='registry.pivotal.io' --docker-username=$TANZU_NET_USER --docker-password=$TANZU_NET_PASS
+kapp deploy -a tap-package-repo -n tap-install -f ./manifests/tap-package-repo.yaml -y
+tanzu package repository list -n tap-install
+echo "TEST OUT"
 
 echo '' >>~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
-
-# Install kapp controller
-echo "Install kapp controller"
-kapp deploy -a kc -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/latest/download/release.yml
 
 # Install app accelerator
 info "Installing app accelerator ..."
