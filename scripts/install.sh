2a39236fc9a2bd4a46d233cb4f396fb0d84c0d9e
Aaron Torgerson <taaron@taaron-a01.vmware.com>
2021-09-02 20:30:21 -0700

    debug

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,7 +40,7 @@ install_tanzu_cli "$UAA_REFRESH_TOKEN" "$TANZU_CLI_VERSION"
 info "Done for now ..."
 install_k3s
 sudo env "PATH=$PATH" kubectl config set-context ${KUBERNETES_CONTEXT}
-sudo chmod 666 ~/.kube/config
+sudo chmod -R 666 ~/.kube
 exit 0
 # Install kapp controller
 echo "Install kapp controller"
