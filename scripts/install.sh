49cb5c2adb12b57ae3b7ddcb95c206a52438efe3
Aaron Torgerson <garreeoke@gmail.com>
2021-09-08 12:55:05 -0700

    Update install.sh
    b

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ YTT_VERSION="v0.36.0"
 IMGPKG_VERSION="v0.17.0"
 KBLD_VERSION="v0.30.0"
 KUBECTL_VERSION=v1.22.0
-UAA_REFRESH_TOKEN="593092fe98754de894f37d4bea612bc4-r" # Make an option
+UAA_REFRESH_TOKEN="" # Make an option
 TANZU_CLI_VERSION="v1.4.0-rc.5" # Make an option
 
 ### Load Helper Functions
