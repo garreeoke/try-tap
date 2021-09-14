9ee2206fc604dcfaec45882fabf91709278d8843
Aaron Torgerson <taaron@taaron-a01.vmware.com>
2021-09-13 19:38:11 -0700

    typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -132,13 +132,13 @@ sleep 5
 # Install TBS
 info "Installing Tanzu Build Service ..."
 # Login to local reg
-docker login ${HARBOR_SVC}:8085
+docker login ${HARBOR_SVC}:8085 -u admin -p 'Harbor12345'
 # Login to pivotal reg
 docker login registry.pivotal.io -u $TANZU_NET_USER -p $TANZU_NET_PASSWORD
 # Copy image from piv to local
-imgpkg copy -b "registry.pivotal.io/build-service/bundle:${TBS_VERSION}" --to-repo ${HARBOR_SVC}:8085/library/build-service
+imgpkg copy -b "registry.pivotal.io/build-service/bundle:${TBS_VERSION}" --to-repo "${HARBOR_SVC}:8085/library/build-service"
 # Deploy
-ytt -f /tmp/bundle/values.yaml -f /tmp/bundle/config/ -v docker_repository='${HARBOR_SVC}:8085/library/build-service' -v docker_username='admin' -v docker_password='Harbor12345' -v tanzunet_username="$TANZU_NET_USER" -v tanzunet_password="$TANZU_NET_PASSWORD" | sudo kbld -f /tmp/bundle/.imgpkg/images.yml | sudo kapp deploy -a tanzu-build-service -f- -y
+ytt -f /tmp/bundle/values.yaml -f /tmp/bundle/config/ -v docker_repository="${HARBOR_SVC}:8085/library/build-service" -v docker_username='admin' -v docker_password='Harbor12345' -v tanzunet_username="$TANZU_NET_USER" -v tanzunet_password="$TANZU_NET_PASSWORD" | sudo kbld -f /tmp/bundle/.imgpkg/images.yml | sudo kapp deploy -a tanzu-build-service -f- -y
 sudo kapp deploy -a tanzu-build-service -f- -y --debug
 # Kp command to see builders
 kp clusterbuilder list
